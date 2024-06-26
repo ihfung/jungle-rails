1ec2026859be23a9824470e5e1a28d844a139656
ihleung <ihfungleung@gmail.com>
2024-06-26 15:30:13 -0400

    Bug Missing Admin Security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"] # This is a basic authentication method that Rails provides. It will prompt the user for a username and password before allowing them to access any actions in this controller. The username and password are stored in the environment variables USERNAME and PASSWORD, which are set in the .env file.
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"] # This is a basic authentication method that Rails provides. It will prompt the user for a username and password before allowing them to access any actions in this controller. The username and password are stored in the environment variables USERNAME and PASSWORD, which are set in the .env file.
   def index
     @products = Product.order(id: :desc).all
   end
