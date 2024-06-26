34e3408facbe388c5a33b1c147183d9c30a4a4ac
ihleung <ihfungleung@gmail.com>
2024-06-26 16:19:38 -0400

    dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"] # This is a basic authentication method that Rails provides. It will prompt the user for a username and password before allowing them to access any actions in this controller. The username and password are stored in the environment variables USERNAME and PASSWORD, which are set in the .env file.
   def show
-    @product_count = Product.count
-    @category_count = Category.count
+    @product_count = Product.count #gets the product count from the database and stores in variable @product_count
+    @category_count = Category.count #gets the category count from the database and stores in variable @category_count
   end
 end
