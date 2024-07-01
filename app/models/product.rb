9a53f1c5e077cb1ca77d472b7603ed5a33fa6665
ihleung <ihfungleung@gmail.com>
2024-07-01 15:34:15 -0400

    updated comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
 
   belongs_to :category
 
+  # Validation for the product model to ensure all fields are filled in and the price is a number greater than 0 and the quantity is a number greater than 0 and the category is filled in as well 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
