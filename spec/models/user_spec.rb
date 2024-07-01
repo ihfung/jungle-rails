1c620e495ddff0e7a23bacda8708b8ee44576fb4
ihleung <ihfungleung@gmail.com>
2024-07-01 17:34:52 -0400

    updated the spec for product and user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,37 +11,37 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
 
-    it "Invalid first name" do 
+    it "validate first name is empty" do 
       @user.first_name = nil
       expect(@user).to be_invalid
     end
 
-    it "Invalid last name" do
+    it "validate last name is empty" do
       @user.last_name = nil
       expect(@user).to be_invalid
     end
 
-    it "Invalid email" do
+    it "validate email is empty" do
       @user.email = nil
       expect(@user).to be_invalid
     end
 
-    it "Invalid password" do
+    it "validate password is empty" do
       @user.password = nil
       expect(@user).to be_invalid
     end
 
-    it "Invalid password confirmation" do
+    it "validate password confirmation is empty" do
       @user.password_confirmation = nil
       expect(@user).to be_invalid
     end
 
-    it "short password" do
+    it "validate for short password" do
       @user.password = "pass"
       expect(@user).to be_invalid
     end
 
-    it "Invalid user with same email" do
+    it "validate different user using the same email" do
       @user.save # save the user
       @user2 = User.new(first_name: "Jane", last_name: "Doe", email: "john@doe.ca", password: "password", password_confirmation: "password")
       expect(@user2).to be_invalid
@@ -51,17 +51,17 @@ RSpec.describe User, type: :model do
 
   describe '.authenticate_with_credentials' do
     # examples for this class method here
-    it "valid user" do
+    it "validate user" do
       @user.save
       expect(User.authenticate_with_credentials("john@doe.ca", "password")).to_not be_nil
     end
 
-    it "valid user with whitespaces" do
+    it "validate user with whitespaces" do
       @user.save
       expect(User.authenticate_with_credentials("     john@doe.ca", "password")).to_not be_nil
     end
     
-    it "valid user with different case" do
+    it "validate user with lower/upper case" do
       @user.save
       expect(User.authenticate_with_credentials("John@Doe.Ca", "password")).to_not be_nil
     end
