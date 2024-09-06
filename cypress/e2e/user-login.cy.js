da81eaab5727f09533fa175b256c7dc6f4390a49
ihleung <ihfungleung@gmail.com>
2024-09-06 12:11:48 -0400

    updated test

--- cypress/e2e/user-login.cy.js
+++ cypress/e2e/user-login.cy.js
@@ -1,22 +1,24 @@
-describe('user-login', () => {
+describe('sign-up', () => {
   
   it('Sign up a user', () => {
     cy.visit('/signup');
-    cy.get('input[name="user[first_name]"]').type('Sakura');
-    cy.get('input[name="user[last_name]"]').type('Haruno');
-    cy.get('input[name="user[email]"]').type('sakura@a.ca');
+    cy.get('input[name="user[first_name]"]').type('Jenson');
+    cy.get('input[name="user[last_name]"]').type('Ackles');
+    cy.get('input[name="user[email]"]').type('jenson@a.ca');
     cy.get('input[name="user[password]"]').type('123456');
     cy.get('input[name="user[password_confirmation]"]').type('123456');
     cy.get('form').click().submit();
-    
+    cy.visit('/');
+    cy.get('a').contains('Logout').click();
   });
-
-  it('should login a user', () => {
-    cy.visit('/login');
-    cy.get('input[name="email"]').type('sakura@a.ca');
-    cy.get('input[name="password"]').type('123456');
-    cy.get('form').submit();
-  }); 
 });
 
-  
+describe('login', () => {
+  it('should login a user', () => {
+    cy.visit('/login');
+    cy.get('input[name="email"]').type('jenson@a.ca');
+    cy.get('input[name="password"]').type('123456');
+    cy.get('form').submit();
+   
+  });
+});
